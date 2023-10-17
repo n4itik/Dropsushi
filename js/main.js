9a0819cfe4247b8ee97fcd2f6414c35280daec34
Sn4pe <hi@sn4pe.com>
2023-10-17 13:18:37 +0200

    fix: style and remove js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var menuIcon = document.getElementById('menu-icon');
-var menu = document.querySelector('.header__menu-mobile');
-
-menuIcon.addEventListener('click', () => {
-    menu.style.display = (menu.style.display === 'none') ? 'flex' : 'none';
-  });
-  
